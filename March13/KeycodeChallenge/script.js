271f8cdcde93d0f2ae79b0d80a5472e62395bf27
Ray Merrell <RayMerrell@outlook.com>
2023-03-13 15:30:26 +0000

    m46Course

---
+++ March13/KeycodeChallenge/script.js
@@ -0,0 +1,9 @@
+const code= document.getElementById("idCode");
+const key = document.getElementById("idKey");
+const which = document.getElementById("idWhich");
+
+document.addEventListener("keypress", (event)=>{
+        code.innerText = "Code:" + event.code;
+        key.innerText = "Key:" + event.key;
+        which.innerText = "Which:" + event.which;
+})
