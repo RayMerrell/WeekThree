b9a92e5bdc493687ac95cc8ce2a83beb05c24566
Ray Merrell <RayMerrell@outlook.com>
2023-03-13 12:36:39 +0000

    M46Course

---
+++ March13/QuerySelector/script.js
@@ -0,0 +1,16 @@
+// const listItems = document.querySelectorAll("li:nth-child(even)");
+
+// console.log(listItems);
+// console.log(listItems.length);
+
+// const listItems = document.querySelectorAll("li");
+
+// for (let x =0; x<listItems.length; x++){
+//     listItems[x].style.color="lightgreen";
+// }
+
+
+// const listItems = document.querySelectorAll("li:nth-child(even)");
+// for (let x =0; x<listItems.length; x++){
+//     listItems[x].style.color="lightgreen";
+// }
---
+++ March13/textContentandinnerHTML/script.js
@@ -0,0 +1,16 @@
+const placeholder = document.getElementById("placeholder");
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const list = document.getElementById("list");
+const image= document.getElementById("image");
+
+submit.addEventListener("click", ()=>{
+//     placeholder.style.color="goldenrod";
+//     //placeholder.textContent = input.value;
+//     placeholder.textContent =placeholder.textContent = input.value;
+    // list.innerHTML= `<li>${input.value}</li>`;
+    image.src="https://4.bp.blogspot.com/-WuqLzs5YlbA/TtANNV38KYI/AAAAAAAADgs/rKODMlsK040/s1600/goat.jpg";
+})
+
+
+
