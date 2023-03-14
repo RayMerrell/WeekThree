cfbdb03554de37cc373cff9a3c4e74b51197e002
Ray Merrell <RayMerrell@outlook.com>
2023-03-14 18:06:32 +0000

    M46Course

--- March14/Drumkit/script.js
+++ March14/Drumkit/script.js
@@ -1,23 +1,15 @@
-const allDivs = document.querySelectorAll('div');
-allDivs.forEach((div)=>{
-    if (div.className==="clsInstrument"){
-        div.addEventListener('click', ()=>{
-            div.lastElementChild.play();
-            animate(div.lastElementChild.id);
-        })
-    };
-});
+for (const div of document.getElementsByClassName("clsInstrument")){
+    div.addEventListener('click', ()=>{
+        play(div.lastElementChild.id);
+    })
+ };
 document.addEventListener('keypress', (event)=>{
-    try{ //fail silently on unbound keypress please
-        document.getElementById(event.key).play();
-        animate(event.key);
-    }catch(e){
-
-    };
+    try{play(event.key.toLowerCase());}catch(e){};//fail silently on undesirable keypress please
 });
-function animate(id){
-    let picdiv = document.getElementsByClassName(id);
-    picdiv[0].style.display='block';
+function play(id){
+    let picdiv = document.getElementsByClassName(id);//returns a collection but we know there is only one element, 
+    picdiv[0].style.display='block';//so use index [0]
+    document.getElementById(id).play();
     setTimeout(() => {
         picdiv[0].style.display='none';
     }, 1200);
